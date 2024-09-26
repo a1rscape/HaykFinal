0ea409e08f5a8710d04b0e775da5204604d1f531
Scape <hamlet.mihranyan@gmail.com>
2024-09-26 23:02:29 +0400

    header v2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,7 +44,15 @@ const Header = () => {
                             <Link to={item.url} style={{ color: 'inherit', textDecoration: 'none' }}>{item.label}</Link>
                         </li>
                     ))}
-                    <button>Schedule a visit</button>
+
+                    <button className="icon-button">
+                        <div className="icon-container">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="24px" height="24px">
+                                <path d="M19 3h-1V1h-2v2H8V1H6v2H5C3.9 3 3 3.9 3 5v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V9h14v10zm-7-9h5v5h-5V10z" />
+                            </svg>
+                        </div>
+                        <span className="button-text">Schedule a visit</span>
+                    </button>
                 </ul>
             </header>
         </>
